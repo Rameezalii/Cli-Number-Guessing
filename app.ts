ae6849984e1fe814097c78a8d2636dd976d494e1
Rameez Ali <ra2252252@gmail.com>
2024-03-23 09:28:10 +0500

    created number guessing game

---
+++ app.ts
@@ -0,0 +1,18 @@
+#! usr/bin/env node 
+import inquirer from "inquirer";
+
+let randomNumber = Math.floor(Math.random() * 6 + 1);
+
+const answer = await inquirer.prompt([
+  {
+    name: "userGuessedNumber",
+    type: "number",
+    message: "Please guessed a number between 1-10",
+  },
+]);
+
+if (answer.userGuessedNumber === randomNumber) {
+  console.log("Congratulation! you guessed right number");
+} else {
+  console.log("you guessed wrong number");
+}
